53be5f330e430b1e27ef01518e25d681d67b8b0f
doni-robert <irobertndungu@gmail.com>
2024-10-22 11:22:46 +0300

    chore: create index view

--- eventshare/event/views.py
+++ eventshare/event/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Event, User
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_event= Event.objects.all().count()
+
+    # Available books (status = 'a')
+
+    # The 'all()' is implied by default.
+    num_users = User.objects.count()
+
+    context = {
+        'num_event': num_event,
+        'num_users': num_users,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
